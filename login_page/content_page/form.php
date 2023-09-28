3983594bbb6ea10cd289fc02f8bc6cc62d08409e
dhaboav <dhaboav@gmail.com>
2023-09-28 21:04:38 +0700

    login session

--- login_page/content_page/form.php
+++ login_page/content_page/form.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+
+// Check if the login session exists
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    // If the session doesn't exist or is not set to true, redirect to the login page
+    header('Location: ../login.php');
+    exit; // Terminate the script
+}
+
+// Logout action
+if (isset($_GET['logout'])) {
+    session_unset();    // Unset all session variables
+    session_destroy();  // Destroy the session
+    header('Location: ../login.php'); // Redirect to the login page
+    exit; // Terminate the script
+}
+?>
 <html lang="id">
 
 <head>
@@ -19,6 +37,7 @@
         <a href="#">Link 3</a>
         <a href="#">Link 4</a>
         <a href="#">Link 5</a>
+        <a href="?logout=1" class="logout">Logout</a>
     </nav>
     <section>
         <h1>FORM</h1>
--- login_page/login.php
+++ login_page/login.php
@@ -1,3 +1,42 @@
+<?php
+session_start();
+
+// Cek jika user sudah login dan session login adalah true
+if (isset($_SESSION['login']) && $_SESSION['login'] === true) {
+    header('Location: content_page/index.php'); // Redirect ke dashboard
+    exit; // Akhiri skrip
+}
+
+// Definisikan daftar user valid dan kata sandi mereka
+$user_list = [
+    ['username' => 'miko', 'password' => '123'],
+    ['username' => 'admin', 'password' => 'admin']
+];
+
+// Cek jika form login telah disubmit
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Dapatkan input user dari form
+    $userID = $_POST['userID'];
+    $password = $_POST['password'];
+
+    // Cek apakah kredensial yang disubmit cocok dengan salah satu user dalam daftar
+    $authenticated = false;
+    foreach ($user_list as $user) {
+        if ($user['username'] === $userID && $user['password'] === $password) {
+            $authenticated = true;
+            $_SESSION['username'] = $userID;
+            $_SESSION['login'] = true;
+            header('Location: content_page/index.php'); // Redirect ke dashboard
+            exit; // Akhiri skrip
+        }
+    }
+
+    // Jika kredensial tidak cocok, set pesan kesalahan
+    $_SESSION['error'] = 'Invalid username or password';
+    header('Location: login.php?error=1'); // Redirect ke halaman login dengan pesan kesalahan
+    exit; // Akhiri skrip
+}
+?>
 <html lang="id">
 
 <head>
@@ -16,7 +55,7 @@
         }
         ?>
 
-        <form action="process.php" method="post">
+        <form action="" method="post">
             <input type="text" name="userID" placeholder="Username" required><br>
             <input type="password" name="password" placeholder="Password" required><br>
             <button type="submit">LOGIN</button>
